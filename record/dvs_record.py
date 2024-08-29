fbd5ed5c896de37d43a8ab388f6421431fbacd05
arshiaesll <arshia.esl83@gmail.com>
2024-08-29 12:41:31 -0400

    adding the label list and fps

--- record/dvs_record.py
+++ record/dvs_record.py
@@ -1,15 +1,22 @@
 import cv2
 import numpy as np
 import argparse
+import time
+
 
 def open_cam():
   print("Opening Camera...",end="")
+  # HAVE CHANGE IT TO 1
   cam = cv2.VideoCapture(0)
   ret, frame = cam.read()
-  
+  # ADDED CODE
+  fps = cam.get(cv2.CAP_PROP_FPS)
+  print("####" + str(fps))
+
+  # ADDED CODE
   if ret: 
     print("Complete")
-    return cam, frame.shape[0], frame.shape[1]
+    return cam, frame.shape[0], frame.shape[1], int(round(fps))
   else: 
     print("Failure")
     print("Please check camera!")
@@ -45,14 +52,20 @@ def checkKey():
 def settings():
 
   print("Hello, Welcome to the DVS Recording Toolbox!")
+  # Needs to be replaced with actual labels
+  label_list = {1:"l1", 2 : "l2", 3 : "l3", 4 : "l4"}
   while (True):
-    num_frames = int(input("How many frames would you like to record?\n>> "))
-    label = int(input("What class label is being recorded?\n>> "))
+    num_sec = int(input("How many seconds would you like to record?\n>> "))
+    print("Which of the following classes is being recorded?")
+    for key, val in label_list.items() :
+      print(str(key) + ") " + val)
+    label_int= int(input())
+    label = label_list[label_int]
 
-    user = input(f'You would like to record {num_frames} frames of class {label}? [y/n]\n>> ')
+    user = input(f'You would like to record {num_sec} seconds of class {label}? [y/n]\n>> ')
     if user.lower() == 'y':
       print("Values accepted!") 
-      return num_frames, label
+      return num_sec, label
     else:
       print("Please reenter values!")
 
@@ -60,14 +73,16 @@ def settings():
 def main():
 
   # User Settings
-  num_frames, label = settings()
+  num_seconds, label = settings()
 
   # Open Camera
-  cam, img_h, img_w = open_cam()
+  cam, img_h, img_w, fps = open_cam()
+  num_frames = num_seconds * fps
+  print("FPS" + str(fps), "number of frame: " + str(num_frames))
   data = np.zeros(shape=(num_frames, 2, img_h, img_w), dtype=bool)
 
   # Set up frames
-  print("Opening Preview [Press to (r)ecord or (q)uit]")
+  print("Opening Preview [Press to (r)ecord or (q)uit. IN THE PREVIEW NOT TERMINAL]")
   _, frame = cam.read()
   prev = np.ones_like(frame) * frame / 255.
   while (True):
@@ -81,6 +96,7 @@ def main():
     if k == ord('r'): break
 
   print("Recording...           ", end="\r")
+  start = time.time() 
   for i in range(num_frames):    
     _, frame = cam.read()
     frame = np.array(frame, np.float32) / 255.
@@ -89,6 +105,8 @@ def main():
     k = checkKey()
     data[i] = spikes
     prev = frame
+  end = time.time()
+  print(end - start, "seconds")
   print("Recording...Complete")
   fn = input("Where would you like to save this data?")
   if fn == "": fn = "./saved_data.npz"
