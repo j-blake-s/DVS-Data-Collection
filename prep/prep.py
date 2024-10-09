777089c586de0fadc621fd5aa0714d5541462d9f
j-blake-s <blakeseekings25@gmail.com>
2024-10-09 10:49:40 -0400

    changed prep

--- prep/prep.py
+++ prep/prep.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import torch
 import torch.nn as nn
-
+import sys
 
 def prep_data(data_dir,save_dir):
 
@@ -17,58 +17,31 @@ def prep_data(data_dir,save_dir):
     x = data['x']
     C, H, W, T = x.shape
 
-
-  # Set up Data
-  xs = np.zeros(shape=(B, C, H, W, T))
-  ys = np.zeros(shape=(B,))
-
-
   # Load Data
   for i, fn in enumerate(dirs):
     with np.load(os.path.join(data_dir,fn)) as data:
-      xs[i] = data['x']
-      ys[i] = data['y']
+      x = data['x']
+      y = data['y']
   
     
-  # Max Pool
-  torch_xs = torch.from_numpy(xs)
-  max_pool = nn.MaxPool3d((2,2,2), (2,2,2))
-  torch_xs = max_pool(torch_xs)
-  xs = torch_xs.numpy()
-  H = H // 2
-  W = W // 2
-  T = T // 2
+    # Max Pool
+    torch_x = torch.from_numpy(x).to(torch.float32)
+    max_pool = nn.MaxPool3d((2,2,2), (2,2,2))
+    torch_x = max_pool(torch_x)
+    x = torch_x.numpy().astype(bool)
+    H = H // 2
+    W = W // 2
+    T = T // 2
 
 
-  # Flip Channels
-  samples = np.zeros(shape=(B*2, C, H, W, T))
-  labels = np.zeros(shape=(B*2,))
+    xc = np.zeros_like(x)
 
-  samples[:B] = xs
-  samples[B:,0,:,:,:] = xs[:,1,:,:,:]
-  samples[B:,1,:,:,:] = xs[:,0,:,:,:]
-
-  labels[:B] = ys
-  labels[B:] = ys
-  B = B*2
-
-  # Shuffle
-  seed = np.random.get_state()
-  np.random.shuffle(samples)
-  np.random.set_state(seed)
-  np.random.shuffle(labels)
+    xc[0] = x[1]
+    xc[1] = x[0]
 
 
-  # Train Test Split
-  split = int(B * 0.8)
+    np.savez_compressed(os.path.join(save_dir, fn), x=x, y=y)
+    np.savez_compressed(os.path.join(save_dir, f'{fn[:-4]}_flip.npz'), x=xc, y=y)
 
-  trainX = samples[:split]
-  trainY = labels[:split]
-
-  testX = samples[split:]
-  testY = labels[split:]
-
-  # Save
-  np.savez_compressed(save_dir, tX=trainX, tY=trainY, vX=testX, vY=testY)
-
-prep_data("/home/seekingj/data/ASL/raw", "/home/seekingj/data/ASL/ASL.npz")
+# prep_data("/home/seekingj/data/ASL/raw", "/home/seekingj/data/ASL/prep")
+prep_data(sys.argv[1],sys.argv[2])
